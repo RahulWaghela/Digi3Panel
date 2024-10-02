001ce5ca718c73ee11b89f813281ada3a1458166
RahulWaghela <112179244+RahulWaghela@users.noreply.github.com>
2024-10-02 03:27:59 -0700

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 // mongoose.connect('mongodb+srv://sejal:PJLYvnOSauidRbgg@cluster0.cyncaox.mongodb.net/?retryWrites=true&w=majority', {
-mongoose.connect('mongodb+srv://RahulWaghela:testuser123@cluster0.wcnvqgq.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://RahulWaghela:physics.h123@cluster0.wcnvqgq.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
